9fb04ca2cf45d2b74be034e5a0cdea3baa2ea144
Bereket <121398009+byergali@users.noreply.github.com>
2023-03-01 21:33:06 +0500

    Update 3.py

--- tsis5 regex/3.py
+++ tsis5 regex/3.py
@@ -2,8 +2,7 @@ import re
 
 #Write a Python program to find sequences of lowercase letters joined with a underscore.
 
-get = open('row.txt', encoding='utf-8')
-text = str(get.read())
+text = input()
 
 
 pattern = r'[a-z]+_*[a-z]+'
