a1fb0a08a5d2ffe32797305726d548ea74331fa6
Bereket <121398009+byergali@users.noreply.github.com>
2023-03-10 19:00:41 +0600

    BY

--- tsis5/date/task 1.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-ans = datetime.date.today() - datetime.timedelta(5)
-print(ans)
--- tsis5/date/task 3.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-ans = datetime.datetime.today().replace(microsecond=0)
-print(ans)
